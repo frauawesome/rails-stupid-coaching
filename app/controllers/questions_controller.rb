c31f67804a0724241b2362c79828334f574dd8a2
Carly Schmitting <cschmitting@gmail.com>
2019-04-15 15:58:06 +0800

    check this stupid coach out

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def stupid_answer(question)
+    if question == 'I am going to work'
+      'Great!'
+    elsif question[-1] == '?'
+      'Silly question, get dressed and go to work!'
+    else
+      'I don\'t care, get dressed and go to work!'
+    end
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = stupid_answer(@question)
+  end
+end
